c9c6b379df21355f398babc706b1d43a04ab4a0b
oroumand <ar.oroumand@gmail.com>
2023-11-30 09:19:31 +0330

    remove domain part 02

--- 06. ImplementingBusinessLogicPart02/PowerStartDDD.DomainModelBuildingBlocks.Aggregates/Aggregates/Message.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PowerStartDDD.DomainModelBuildingBlocks.Aggregates.Aggregates
-{
-    public class Message(string name,string message)
-    {
-        public readonly string Name = name;
-        public readonly string Value = message;
-    }
-}
